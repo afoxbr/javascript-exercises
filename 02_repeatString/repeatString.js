06ca1fd282fa55277b8a088ddc9cbdbb96000f03
AntonyFB <antonyrbramwell@gmail.com>
2022-06-12 00:32:54 +0100

    Complete exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if (num >= 0) {
+    return string.repeat(num);
+  }
+  else {
+    return "ERROR";
+  }
+  num = 0;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
