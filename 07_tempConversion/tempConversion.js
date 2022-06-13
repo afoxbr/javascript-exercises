7bfcd53f326c9e2eccf3b51c7ab37313dc47f919
AntonyFB <antonyrbramwell@gmail.com>
2022-06-13 21:24:46 +0100

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = (f - 32) * (5 / 9);
+  cRound = Math.round(c * 10) / 10;
+  return cRound;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = (c * (9 / 5)) + 32;
+  fRound = Math.round(f * 10) /10;
+  return fRound;
 };
 
 // Do not edit below this line
