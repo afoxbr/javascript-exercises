cbaa5b5b84076a6eff81b52414e1bdc3fde402fb
AntonyFB <antonyrbramwell@gmail.com>
2022-06-27 19:15:26 +0100

    Exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	c = a + b;
+  return c;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	ans = a - b;
+  return ans;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = array[0];
+  for (let i = 1; i < array.length; i++) {
+    product = (product * array[i]);
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return (a**b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let facto = num;
+  if (num === 0 || num === 1){
+    return 1;
+  }
+  while (num > 1) {
+    num--;
+    facto = facto * num;
+  }
+  return facto;
 };
 
 // Do not edit below this line
