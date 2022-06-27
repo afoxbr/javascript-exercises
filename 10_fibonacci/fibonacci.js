231c10302b42e0026c871b35d08799ec6ae6b5d7
AntonyFB <antonyrbramwell@gmail.com>
2022-06-27 19:57:46 +0100

    Fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num < 0) {
+    return "OOPS";
+  } else {
+    let fib = [0,1];
+    for (let i = fib.length; i < 30; i++) {
+      fib[i] = fib[i-2] + fib[i-1];
+    };
+    return fib[num];
+    };
 };
 
 // Do not edit below this line
