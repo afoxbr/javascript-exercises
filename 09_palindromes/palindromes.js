ad72bb84ab1df6b5b72d5b2ced03364a637fab40
AntonyFB <antonyrbramwell@gmail.com>
2022-06-27 19:37:22 +0100

    Exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strLow = str.toLowerCase();
+    let regex = strLow.replace(/[^a-z]+/g, '');
+    let reversed = regex.split("").reverse().join("")
+    return regex === reversed
 };
 
 // Do not edit below this line
